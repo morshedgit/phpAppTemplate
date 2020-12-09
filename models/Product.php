da0651299a8c5c54d49a1ed387b793751bd1ea2d
unknown <aradontheline@gmail.com>
2020-12-08 20:47:00 -0800

    3

--- models/Product.php
+++ models/Product.php
@@ -45,13 +45,13 @@ class Product
         $this->height = htmlspecialchars(strip_tags($this->height));
         $this->location = htmlspecialchars(strip_tags($this->location));
 
-        // $this->id??10;
-        // $this->name??"Fork";
-        // $this->location??"A4";
-        // $this->width??10;
-        // $this->length??10;
-        // $this->height??10;
-        // $this->weith??10;
+        $this->id??10;
+        $this->name??"Fork";
+        $this->location??"A4";
+        $this->width??10;
+        $this->length??10;
+        $this->height??10;
+        $this->weith??10;
 
         // if (empty( $this->id ) ) { $this->id = 10;}
         // if (empty( $this->name ) ) { $this->name = "Fork";}
@@ -62,15 +62,11 @@ class Product
         // if (empty( $this->weight ) ) { $this->weight = 10;}
 
 
-        //create query
-        // $query = 'INSERT INTO ' . $this->table . ' (id,name,location,width,length,height,weight) 
-        //     VALUES ('.$this->id.','.$this->name.','.$this->location.','.$this->width.','.$this->length.','.$this->height.','.$this->weight.')';
+        //create query    VALUES ('.$this->id.','.$this->name.','.$this->location.','.$this->width.','.$this->length.','.$this->height.','.$this->weight.')';
 
         $query = "INSERT INTO product (id,name,location,width,length,height,weight) VALUES (".$this->id.",'".$this->name."','".$this->location."',".$this->width.",".$this->length.",".$this->height.",".$this->weight.")";
-        // $query = "INSERT INTO product (id,name,location,width,length,height,weight) VALUES (%,%,%,%,%,%,%)";
-        return $query;
         //Prepare
-        // $stmt = $this->conn->prepare($query);
+        $stmt = $this->conn->prepare($query);
 
         //Bind data
         // $stmt->bindParams(':name', $this->name);
@@ -82,10 +78,10 @@ class Product
         // $stmt->bindParams(':weight', $this->weight);
 
         //Execute query
-        // if ($stmt->execute()) {
-        //     return true;
-        // }
+        if ($stmt->execute()) {
+            return true;
+        }
 
-        // printf("Error: %s.\n", $stmt->error);
+        printf("Error: %s.\n", $stmt->error);
     }
 }
