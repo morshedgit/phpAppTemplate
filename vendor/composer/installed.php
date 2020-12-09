eaff5c2df88137b7fbef292cfa4078a4d2af0646
unknown <aradontheline@gmail.com>
2020-12-08 16:01:55 -0800

    3

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '99cd1f655ea2033a2dc5819b131ec2c617ddbfee',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '99cd1f655ea2033a2dc5819b131ec2c617ddbfee',
+    ),
+  ),
+);
