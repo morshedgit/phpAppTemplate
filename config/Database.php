158b50cb02433e41c4e21cbdc21a1ec6968b927c
Sadeq <sadeq.morshedloo@gmail.com>
2021-02-15 15:05:41 -0800

    all

--- config/Database.php
+++ config/Database.php
@@ -1,24 +1,32 @@
 <?php
-    class Database {
-        //DB Params
-        private $host = 'us-cdbr-east-02.cleardb.com';
-        private $db_name = 'heroku_ecbceff60760a98';
-        private $username = 'bf3778cfcc1441';
-        private $password = 'c1ab19fe';
-        private $conn;
+class Database
+{
+    //DB Params
+    private $host = 'getenv("HOST")';
+    private $db_name = 'getenv("DB_NAME")';
+    private $username = 'getenv("DB_USERNAME")';
+    private $password = 'getenv("DB_PASSWORD")';
+    private $conn;
 
-        //DB Connect
-        public function connect(){
-            $this->conn = null;
-            $this->conn = new PDO('mysql:host='.$this->host.';dbname='.$this->db_name,$this->username,$this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
-            try{
+    public function __construct()
+    {
+        $this->host = getenv("HOST");
+        $this->db_name = getenv("DB_NAME");
+        $this->username = getenv("DB_USERNAME");
+        $this->password = getenv("DB_PASSWORD");
+    }
 
-            }catch(PDOException $e){
-                echo 'Connection Error: '.$e->getMessage();
-            }
-
-            return $this->conn;
+    //DB Connect
+    public function connect()
+    {
+        $this->conn = null;
+        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        try {
+        } catch (PDOException $e) {
+            echo 'Connection Error: ' . $e->getMessage();
         }
 
+        return $this->conn;
     }
+}
